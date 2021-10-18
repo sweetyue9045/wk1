bfb5d41195d5bef979a19b604e7e6c0ce53482af
sweetyue9045 <sweetyue9045@gmail.com>
2021-10-18 09:38:12 +0800

    add home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
-import Header from "./components/Header"
-import Nav from "./components/Nav"
-import Content from "./components/Content"
-import Footer from "./components/Footer"
+import HOME from './pages/Home'
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <Content />
-      <Footer />
-    </div>
+    <HOME />
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Nav from "../components/Nav"
+import Content from "../components/Content"
+import Footer from "../components/Footer"
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            <Nav />
+            <Content />
+            <Footer />
+        </div>
+    );
+}
